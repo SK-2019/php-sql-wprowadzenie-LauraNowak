65fb4471c8fff011f2e1a212aba8606b529efd6b
LauraNowak <73532414+LauraNowak@users.noreply.github.com>
2020-12-09 16:56:00 +0100

    .

--- index.php
+++ index.php
@@ -1,25 +1,18 @@
 <!DOCTYPE html>
-
 <html>
 <head>
-  <div class="container">
-      <div class="item">
-        banner
-        <h1 class="tltle">LauraNowak</h1>
-      </div>
-      <div class="item_colorBlue">Menu Boczne</div>
-      <div class="item_colorGreen">Treść Strony</div>
-    </div>
-<link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css">
+        
+</head>
+<body>
 <div class="button"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-LauraNowak">Github</a></div>
-
 
 <div class="nav">
     <a class="nav_link" href="pracownicy.php">Pracownicy</a>
     <p class="space"></p>
     <a class="nav_link" href="funkcjeAgregujace.php">Funkcje Agregujace</a>
     <p class="space"></p>
-    <a class="nav_link" href="index.php">Strona głuwna</a>
+    <a class="nav_link" href="index.php">Strona główna</a>
     <p class="space"></p>
     <a class="nav_link" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
     <p class="space"></p>
@@ -35,20 +28,142 @@
     <p class="space"></p>
     <a class="nav_link" href="wyporzyczanie.php">Wyporzyczanie</a>
 </div>
-  
-  </head>
- <hr></hr>
-<body>
-  
 
-<h1>STRONA GŁÓWNA</h1>
-  
-  
+
+<h1> Strona główna </h1>
+<h2> Laura Nowak 2Ti gr 2 nr 23 </h2>
+
+
 <?php
-  
-  echo("<p>HEJ!</p>");
+
+$conn = new mysqli( "mysql-lauranowak.alwaysdata.net" , "220134" , "Haslo123" , "lauranowak_23");
+$sql = 'SELECT * FROM pracownicy';
+
+       
+        echo("<p>ZADANIE 1</p>");
+$result = $conn->query($sql);
+        echo("<table>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data urodzenia</th>");
+
+         while($row = $result->fetch_assoc()) {
+                echo("<tr>");
+                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>");
+                echo("</tr>");
+    }
+        echo("</table>");
+
+        echo("<p>ZADANIE 2</p>");
+       
+
+        $conn = new mysqli( "mysql-lauranowak.alwaysdata.net" , "220134" , "Haslo123" , "lauranowak_23");
+$result = $conn->query('SELECT * FROM pracownicy where imie like "%a"');
+        
+        echo("<table>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+                while($row = $result->fetch_assoc()) {
+        echo("<tr>");
+        echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+        echo("</tr>");
+    }
+        echo("</table>");
+
+       
+        echo("<p>ZADANIE 3</p>");
+      
+
+        $conn = new mysqli( "mysql-lauranowak.alwaysdata.net" , "220134" , "Haslo123" , "lauranowak_23");
+$result = $conn->query('SELECT * FROM pracownicy where imie like "%a" and (dzial=1 or dzial=4)');
+        echo("<table>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+                while($row = $result->fetch_assoc()) {
+        echo("<tr>");
+        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+
+        echo("</tr>");
+    }
+        echo("</table>");
+
+
+
+
+
+
+    echo("<p>ZADANIE 4</p>");        
+    $conn = new mysqli( "mysql-lauranowak.alwaysdata.net" , "220134" , "Haslo123" , "lauranowak_23");
+        $result = $conn->query('SELECT imie, dzial, zarobki, data_urodzenia, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org and zarobki<30 and (dzial=1 or dzial=2)');
+            echo("<table>");
+            echo("<th>Imie</th>");
+            echo("<th>Dzial</th>");
+            echo("<th>Zarobki</th>");
+            echo("<th>Data_Urodzenia</th>");
+                while($row=$result->fetch_assoc()){ 
+            echo("<tr>");
+                echo("<td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+            echo("</tr>");
+                }
+            echo("</table>");
+                    
+
+    echo("<p>ZADANIE 5</p>");
+    $conn = new mysqli( "mysql-lauranowak.alwaysdata.net" , "220134" , "Haslo123" , "lauranowak_23");
+        $result = $conn->query('SELECT dzial, sum(zarobki) as suma, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org group by dzial');
+            echo("<table>");
+            echo("<th>dzial</th>");
+            echo("<th>suma</th>");
+            echo("<th>nazwa dzialu</th>");
+                while($row=$result->fetch_assoc()){ 
+            echo("<tr>");
+                echo("<td>".$row["dzial"]."</td><td>".$row["suma"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+            echo("</tr>");
+            }
+            echo("</table>");
+        
+
+    echo("<p>ZADANIE 6</p>");
+    $conn = new mysqli( "mysql-lauranowak.alwaysdata.net" , "220134" , "Haslo123" , "lauranowak_23");
+        $result = $conn->query('SELECT dzial, sum(zarobki) as suma, avg(zarobki) as srednia, min(zarobki) as min, max(zarobki) as max, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org group by dzial');
+            echo("<table>");
+            echo("<th>Dział</th>");
+            echo("<th>Suma</th>");
+            echo("<th>Średnia</th>");
+            echo("<th>Min</th>");
+            echo("<th>Max</th>");
+                while($row = $result->fetch_assoc()) {
+            echo("<tr>");
+                echo("<td>".$row['dzial']."</td><td>".$row['suma']."</td><td>".$row['srednia']."</td><td>".$row['min']."</td><td>".$row['max']."</td>");
+            echo("</tr>");
+            }
+            echo("</table>");
+        
+        
+    echo("<p>ZADANIE 7</p>");
+    $conn = new mysqli( "mysql-lauranowak.alwaysdata.net" , "220134" , "Haslo123" , "lauranowak_23");
+        $result = $conn->query('SELECT imie, dzial, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org and dzial = 1 and zarobki between 20 and 40');
+            echo("<table>");
+            echo("<th>Imie</th>");
+            echo("<th>Dział</th>");
+            echo("<th>Zarobki</th>");
+            echo("<th>Data_Urodzenia</th>");
+            echo("<th>nazwa dzialu</th>");
+                while($row = $result->fetch_assoc()) {
+            echo("<tr>");
+                echo("<td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+            echo("</tr>");
+            }
+            echo("</table>");
 
 ?>
-  
-    </body>
+
+</body>
 </html>
