857c58d7e642728a1c4ade5821ba2b5b2a3fa9c6
LauraNowak <73532414+LauraNowak@users.noreply.github.com>
2021-01-26 14:40:21 +0100

    .

--- Biblioteka/ksiazki.php
+++ Biblioteka/ksiazki.php
@@ -28,7 +28,7 @@
   $sql1 = ("SELECT * FROM bibliotekaAutor"); 
   $sql2 = ("SELECT * FROM bibliotekaTytul"); 
   $result=$conn->query($sql1);
-  echo("<form action='wyporzyczanie.php' method='POST'>");
+  echo("<form action='/Biblioteka/wyporzyczanie.php' method='POST'>");
   echo("<label for='Autor'><p>WYBIERZ AUTORA:</p></label>");
   echo("<select name='Autor' id='id_autor'>");
   while($row=$result->fetch_assoc()) 
