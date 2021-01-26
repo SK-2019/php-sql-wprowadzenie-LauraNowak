7f1478c15b2a8648a507a75830d7be111052eef2
LauraNowak <73532414+LauraNowak@users.noreply.github.com>
2021-01-26 14:19:21 +0100

    .

--- Pracownicy/pracownicy.php
+++ Pracownicy/pracownicy.php
@@ -16,14 +16,13 @@
 <li class="nav_link"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-LauraNowak">Github</a></li>
 <li class="nav_link"><a href="pracownicy.php">Pracownicy</a></li>
 <li class="nav_link"><a href="funkcjeAgregujace.php">Funkcje Agregujace</a></li>
-<li class="nav_link"><a href="index.php">Strona główna</a></li>
 <li class="nav_link"><a href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a></li>
 <li class="nav_link"><a href="dataiczas.php">Data i czas</a></li>
-<li class="nav_link"><a href="formularz.html">Formularz</a></li>
 <li class="nav_link"><a href="daneDoBazy.php">Dane Do Bazy</a></li>
-<li class="nav_link"><a href="function.php">Function</a></li>
-<li class="nav_link"><a href="ksiazki.php">Książki</a></li> 
-<li class="nav_link"><a href="wyporzyczanie.php">Wyporzyczanie</a></li>
+<li class="nav_link"><a href="insert.php">Insert</a></li>
+<li class="nav_link"><a href="delete.php">Delete</a></li>
+
+
 </ul>
 </div>
 
