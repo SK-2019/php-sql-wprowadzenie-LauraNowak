65b9516df83786cf2bf99bf53628811b56eae6bd
LauraNowak <73532414+LauraNowak@users.noreply.github.com>
2021-02-23 14:02:23 +0100

    .

--- assets/connect.php
+++ assets/connect.php
@@ -1,14 +1,17 @@
 <?php
 
-$servername = "mysql-lauranowak-23.alwaysdata.net";
-$username = "225095";
-$password = "Haslo123";
-$dbname = "lauranowak-23_23";
+<?php
+$servername = $_SERVER['servername'];
+$username = $_SERVER['username'];
+$password = $_SERVER['password'];
+$dbname = $_SERVER['dbname']; 
 
+// Create connection (łącze się z moją bazą danych)
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-$conn=new mysqli($servername, $username, $password, $dbname);
-        if($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
+// Check connection (sprawdzam połączenie z bazą danych)
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 ?>
 
