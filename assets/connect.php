da82036971c7421ab1afee3f714e421f70cef4b1
LauraNowak <73532414+LauraNowak@users.noreply.github.com>
2021-02-23 14:07:23 +0100

    .

--- assets/connect.php
+++ assets/connect.php
@@ -1,4 +1,3 @@
-
 <?php
 $servername = $_SERVER['servername'];
 $username = $_SERVER['username'];
@@ -14,4 +13,17 @@ if ($conn->connect_error) {
 }
 ?>
 
+<?php
+
+//$servername = "mysql-lauranowak-23.alwaysdata.net";
+//$username = "225095";
+//$password = "Haslo123";
+//$dbname = "lauranowak-23_23";
+
+
+//$conn=new mysqli($servername, $username, $password, $dbname);
+        //if($conn->connect_error) {
+            //die("Connection failed: " . $conn->connect_error);
+        //}
+?>
 
